e0a57c1aaba88777f280c315d207a6231ecec3bb
linle <linle.job@gmail.com>
2015-10-27 23:31:50 -0700

    model of feed

---
+++ app/src/main/java/androidpath/ll/zhihukun/feed.java
@@ -0,0 +1,25 @@
+package androidpath.ll.zhihukun;
+
+/**
+ * Created by Le on 2015/10/26.
+ */
+public class feed {
+    private String title;
+    private String id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
