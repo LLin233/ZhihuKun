f048363fc7ae307ee12f673682a1ebd39b4c42ee
linle <linle.job@gmail.com>
2015-11-03 21:52:26 -0800

    put API URL into class Constants as a static constant

---
+++ app/src/main/java/androidpath/ll/zhihukun/Constants.java
@@ -0,0 +1,9 @@
+package androidpath.ll.zhihukun;
+
+/**
+ * Created by Le on 2015/11/3.
+ */
+public class Constants {
+    public static String BASE_API_URL = "https://news-at.zhihu.com";
+    public static String LASTEST_NEWS_API_URL = "/api/4/news/latest";
+}
